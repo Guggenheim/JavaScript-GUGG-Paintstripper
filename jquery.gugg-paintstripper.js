30c4937c904357109caf3dc3973c8c97795f1f7e
Sean Redmond <github-smr@sneakemail.com>
2012-09-27 11:25:07 -0400

    Lint'd

--- jquery.gugg-paintstripper.js
+++ jquery.gugg-paintstripper.js
@@ -1,17 +1,24 @@
 (function ($) {
   "use strict";
 
-  var methods = {
+  var ps_w,
+    ps_h,
+    ps_orientation,
+    ps_ratio,
+    ps_real_w,
+    ps_real_h,
+    methods = {
       init : function (options) {
         options = $.extend({}, options);
         var lmnt = $(this),
           base_img = lmnt.find('img').first(),
           img_css = {
-            width: 'auto', 
-            height: 'auto', 
+            width: 'auto',
+            height: 'auto',
             position: 'absolute',
-            top: 0, 
-            left: 0};
+            top: 0,
+            left: 0
+          };
 
         if (options.hasOwnProperty('width')) {
           ps_w = options.width;
@@ -30,16 +37,16 @@
 
         if (ps_real_h >= ps_real_w) {
           ps_orientation = 'portrait';
-          ps_ratio = ps_h/ps_real_h;
+          ps_ratio = ps_h / ps_real_h;
           img_css.width = ps_real_w * ps_ratio;
           img_css.height = ps_h;
-          img_css.left = (ps_w - img_css.width)/2;
+          img_css.left = (ps_w - img_css.width) / 2;
         } else {
           ps_orientation = 'landscape';
-          ps_ratio = ps_w/ps_real_w;
+          ps_ratio = ps_w / ps_real_w;
           img_css.width = ps_w;
           img_css.height = ps_real_h * ps_ratio;
-          img_css.top = (ps_h - img_css.height)/2;
+          img_css.top = (ps_h - img_css.height) / 2;
         }
 
         lmnt.find('img').css(img_css);
@@ -48,13 +55,7 @@
       destroy : function () {
         //destroy
       }
-    },
-    ps_w,
-    ps_h,
-    ps_orientation,
-    ps_ratio,
-    ps_real_w,
-    ps_real_h;
+    };
 
   $.fn.paintstripper = function (method) {
     if (methods[method]) {
