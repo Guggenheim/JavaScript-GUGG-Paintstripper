cc4db0be6cdfa1b6262c0bca3bd22589256481fa
Sean Redmond <github-smr@sneakemail.com>
2012-10-11 10:53:40 -0400

    Refactored as a jQuery UI Widget
    The need for some really private methods became pressing and after
    looking at a few patterns, I just decided to go with the widget.

--- jquery.gugg-paintstripper.js
+++ jquery.gugg-paintstripper.js
@@ -35,336 +35,342 @@
 (function ($) {
   "use strict";
 
-  var ps_w,
-    ps_h,
-    ps_orientation,
-    ps_ratio,
-    ps_real_w,
-    ps_real_h,
-    ps_base = $('<div/>').addClass('ps-base ps-platen'),
-    ps_shade = $('<div/>').addClass('ps-shade'),
-    ps_platen = $('<div/>').addClass('ps-platen'),
-    ps_platens = [ps_base, ps_platen],
-    ps_handle = $('<div/>').addClass('ps-handle'),
-    has_draggable = $.fn.draggable !== undefined,
-    methods = {
-      init : function (options) {
-        options = $.extend({}, options);
-        return this.each(function () {
-          var lmnt = $(this),
-            base_img = lmnt.find('img').first(),
-            base = ps_base,
-            stripper = ps_shade,
-            img_css = {
-              width: 'auto',
-              height: 'auto',
-              position: 'absolute',
-              top: 0,
-              left: 0
-            };
+  $.widget('gugg.paintstripper', {
+    options: {
+      x: 1
+    },
+    ps_w: undefined,
+    ps_h: undefined,
+    ps_orientation: undefined,
+    ps_ratio: undefined,
+    ps_real_w: undefined,
+    ps_real_h: undefined,
+    ps_images: undefined,
+    ps_base: $('<div/>').addClass('ps-base ps-platen'),
+    ps_shade: $('<div/>').addClass('ps-shade'),
+    ps_platen: $('<div/>').addClass('ps-platen'),
+    ps_platens: undefined, //[this.ps_base, this.ps_platen],
+    ps_handle: $('<div/>').addClass('ps-handle'), //undefined,
+    has_draggable: $.fn.draggable !== undefined,
 
-          lmnt.save_style();
-          lmnt.addClass('ps-paintstripper');
+    _create: function () {
+      // options = $.extend({}, options);
+      // return this.each(function () {
+        var self = this,
+          lmnt = self.element,
+          ps_images = lmnt.find('img'),
+          base_img = $(ps_images[0]),
+          img_css = {
+            width: 'auto',
+            height: 'auto',
+            position: 'absolute',
+            top: 0,
+            left: 0
+          };
 
-          if (options.hasOwnProperty('width')) {
-            ps_w = options.width;
-            lmnt.width(ps_w);
-          } else {
-            ps_w = lmnt.width();
-          }
+        self.lmnt = lmnt;
+        self.ps_images = lmnt.find('img');
+        base_img = $(self.ps_images[0])
+        self.ps_base = $('<div/>').addClass('ps-base ps-platen');
 
-          if (options.hasOwnProperty('height')) {
-            ps_h = options.height;
-            lmnt.height(ps_h);
-          } else {
-            ps_h = lmnt.height();
-          }
+        self._save_style(lmnt);
+        lmnt.addClass('ps-paintstripper');
 
-          ps_real_w = base_img.width();
-          ps_real_h = base_img.height();
+        if (self.options.hasOwnProperty('width')) {
+          ps_w = options.width;
+          lmnt.width(ps_w);
+        } else {
+          self.ps_w = lmnt.width();
+        }
 
-          if (ps_real_h >= ps_real_w) {
-            ps_orientation = 'portrait';
-            ps_ratio = ps_h / ps_real_h;
-            img_css.width = ps_real_w * ps_ratio;
-            img_css.height = ps_h;
-            img_css.left = (ps_w - img_css.width) / 2;
-          } else {
-            ps_orientation = 'landscape';
-            ps_ratio = ps_w / ps_real_w;
-            img_css.width = ps_w;
-            img_css.height = ps_real_h * ps_ratio;
-            img_css.top = (ps_h - img_css.height) / 2;
-          }
+        if (self.options.hasOwnProperty('height')) {
+          ps_h = options.height;
+          lmnt.height(ps_h);
+        } else {
+          self.ps_h = lmnt.height();
+        }
 
-          lmnt.find('img').save_style().css({
-            width: img_css.width,
-            height: img_css.height
-          });
+        self.ps_real_w = base_img.width();
+        self.ps_real_h = base_img.height();
 
-          lmnt.append(
-            base.append(
+        if (self.ps_real_h >= self.ps_real_w) {
+          self.ps_orientation = 'portrait';
+          self.ps_ratio = self.ps_h / self.ps_real_h;
+          img_css.width = self.ps_real_w * self.ps_ratio;
+          img_css.height = self.ps_h;
+          img_css.left = (self.ps_w - img_css.width) / 2;
+        } else {
+          self.ps_orientation = 'landscape';
+          self.ps_ratio = self.ps_w / self.ps_real_w;
+          img_css.width = self.ps_w;
+          img_css.height = self.ps_real_h * self.ps_ratio;
+          img_css.top = (self.ps_h - img_css.height) / 2;
+        }
+
+        self._save_style(lmnt.find('img')).css({
+          width: img_css.width,
+          height: img_css.height
+        });
+
+        lmnt.append(
+          self.ps_base.append(
+            $('<div/>')
+              .addClass('ps-axle')
+              .css(img_css)
+              .append(base_img.detach())
+          ).css({
+            width: self.ps_w,
+            height: self.ps_h,
+            top: 0,
+            left: 0,
+            position: 'absolute'
+          })
+
+        );
+
+        lmnt.append(
+          self.ps_shade.append(
+            self.ps_platen.append(
               $('<div/>')
                 .addClass('ps-axle')
                 .css(img_css)
-                .append(base_img.detach())
+                .append(lmnt.children('img').detach())
             ).css({
-              width: ps_w,
-              height: ps_h,
+              width: self.ps_w,
+              height: self.ps_h,
               top: 0,
               left: 0,
               position: 'absolute'
             })
+          ).css({
+            width: 0,
+            height: self.ps_h,
+            top: 0,
+            left: 0,
+            overflow: 'hidden',
+            position: 'absolute'
+          })
+        );
 
-          );
+        self.ps_platens = [self.ps_base, self.ps_platen];
+        self._init_draggable(lmnt);
 
-          lmnt.append(
-            stripper.append(
-              ps_platen.append(
-                $('<div/>')
-                  .addClass('ps-axle')
-                  .css(img_css)
-                  .append(lmnt.children('img').detach())
-              ).css({
-                width: ps_w,
-                height: ps_h,
-                top: 0,
-                left: 0,
-                position: 'absolute'
-              })
-            ).css({
-              width: 0,
-              height: ps_h,
-              top: 0,
-              left: 0,
-              overflow: 'hidden',
-              position: 'absolute'
-            })
-          );
+    },
 
-          lmnt.init_draggable();
+    activate: function (id) {
+      var self = this;
 
-        });
-      },
+      self.element.addClass('has-active');
+      self._activate_layer(self.ps_shade.find('img#' + id));
+      return this;
+    },
 
-      activate: function (id) {
-        this.addClass('has-active');
-        ps_shade.find('img#' + id).activate_layer();
+    deactivate: function () {
+      var self = this;
+      self.ps_shade
+        .css({width: 0})
+        .find('img').removeClass('active');
+      return self.element.removeClass('has-active');
+    },
+
+    max_zoom: function () {
+      return 1 / this.ps_ratio;
+    },
+
+    move: function (coord) {
+      var self = this;
+
+      if (coord === undefined) {
+        return self.ps_base.position();
+      }
+
+      coord = $.extend(self.ps_base.position(), coord);
+      $.each(self.ps_platens, function () {
+        this.css(coord);
+      });
+
+      return this;
+    },
+
+    ratio: function () {
+      return this.ps_ratio;
+    },
+
+    reset: function (duration) {
+      var self = this,
+        theta = self.rotate();
+
+      duration = (duration === undefined) ? 0 : duration;
+
+      self.reveal(0, duration);
+      self.zoom(100, duration);
+
+      // Return to home position
+      $.each(self.ps_platens, function () {
+        $(this).css({top: 0, left: 0});
+      });
+
+      // deactivate windowshade
+      self.deactivate();
+
+      // Rotate back to 12 o'clock in the shortest direction
+      if (theta > 180) {
+        self.rotate('+=' + (360 - theta), duration);
+      } else {
+        self.rotate('-=' + theta, duration);
+      }
+
+      return this;
+    },
+
+    reveal: function (amount, duration, easing, callback) {
+      var self = this,
+        lmnt = self.element,
+        frame_w = lmnt.width(),
+        m;
+
+      if (!lmnt.hasClass('has-active')) {
         return this;
-      },
+      }
 
-      deactivate: function () {
-        ps_shade
-          .css({width: 0})
-          .find('img').removeClass('active');
-        return $(this).removeClass('has-active');
-      },
+      if (amount === undefined) {
+        return self.ps_shade.width();
+      }
 
-      move: function (coord) {
-        if (coord === undefined) {
-          return ps_base.position();
+      if ((typeof amount === 'string') &&
+          ((m = amount.match(/^(\d+)\%/)) !== null)) {
+        amount = frame_w * (m[1] / 100);
+      } else if (typeof amount === 'number') {
+        if (amount < 0) {
+          amount = 0;
+        } else if (amount > frame_w) {
+          amount = frame_w;
         }
+      }
 
-        coord = $.extend(ps_base.position(), coord);
-        $.each(ps_platens, function () {
-          $(this).css(coord);
-        });
+      return self.ps_shade.animate({width: amount}, 
+        {duration: duration, easing: easing, step: self._sync_handle, complete: self._sync_handle});
+    },
 
-        return this;
-      },
+    rotate: function (deg, duration, easing, callback) {
+      var self = this,
+        axles = self.element.find('.ps-axle');
 
-      rotate: function (deg, duration, easing, callback) {
-        var axles = $(this).find('.ps-axle');
-        if (deg === undefined) {
-          return parseInt(axles.first().css('rotate'), 10);
-        }
+      console.log(deg);
 
-        return axles.each(function () {
-          $(this).transition({ rotate: deg }, duration, easing,
-            function (callback) {
-              // Check to see if we've gone all the way around in either 
-              // direction
-              var theta = parseInt($(this).css('rotate'), 10);
-              if (theta >= 360) {
-                $(this).css({ rotate: theta % 360 });
-              } else if (theta < 0) {
-                $(this).css({ rotate: (theta % 360) + 360 });
-              }
-              if (callback !== undefined) {
-                callback();
-              }
-            });
-        });
-      },
+      if (deg === undefined) {
+        return parseInt(axles.first().css('rotate'), 10);
+      }
 
-      reveal: function (amount, duration, easing, callback) {
-        if (!this.hasClass('has-active')) {
-          return this;
-        }
-
-        var frame_w = $(this).width(),
-          m;
-
-        if (amount === undefined) {
-          return ps_shade.width();
-        }
-
-        if ((typeof amount === 'string') &&
-            ((m = amount.match(/^(\d+)\%/)) !== null)) {
-          amount = frame_w * (m[1] / 100);
-        } else if (typeof amount === 'number') {
-          if (amount < 0) {
-            amount = 0;
-          } else if (amount > frame_w) {
-            amount = frame_w;
-          }
-        }
-
-        return ps_shade.animate({width: amount}, duration, easing,
-          function () {
-            var current = ps_shade.width();
-            if (current < 0) {
-              current = 0;
-            } else if (current > frame_w) {
-              current = frame_w;
+      return axles.each(function () {
+        $(this).transition({ rotate: deg }, duration, easing,
+          function (callback) {
+            // Check to see if we've gone all the way around in either 
+            // direction
+            var theta = parseInt($(this).css('rotate'), 10);
+            if (theta >= 360) {
+              $(this).css({ rotate: theta % 360 });
+            } else if (theta < 0) {
+              $(this).css({ rotate: (theta % 360) + 360 });
             }
-
-            ps_shade.width(current);
-            if (has_draggable) {
-              ps_handle.css({left: current - (ps_handle.outerWidth() / 2)});
-            }
-
             if (callback !== undefined) {
               callback();
             }
           });
-      },
+      });
+    },
 
-      reset: function () {
-        var lmnt = this,
-          theta = lmnt.paintstripper('rotate');
-
-        // Close windowshade
-        lmnt.paintstripper('reveal', 0);
-
-        // Reset zoom
-        methods.zoom(100);
-
-        // Return to home position
-        $(ps_platens).each(function () {
-          $(this).css({top: 0, left: 0});
-        });
-
-        // deactivate windowshade
-        methods.deactivate();
-
-        // Rotate back to 12 o'clock in the shortest direction
-        if (theta > 180) {
-          lmnt.paintstripper('rotate', '+=' + (360 - theta));
-        } else {
-          lmnt.paintstripper('rotate', '-=' + theta);
-        }
-
-        return this;
-      },
-
-      zoom: function (zoom) {
-        if (zoom === undefined) {
-          return $(".ps-platen img").first().scale();
-        }
-
-        $(".ps-platen img").each(function () {
-          $(this).transition({ scale: (zoom / 100)}, 0);
-        });
-      },
-
-      max_zoom: function () {
-        return 1 / ps_ratio;
-      },
-
-      ratio: function () {
-        return ps_ratio;
-      },
-
-      destroy : function () {
-        var lmnt = $(this);
-
-        // return elements to original style
-        lmnt.find('img').each(function () {
-          $(this).attr('style', $(this).data('orig-style'));
-          $(this).removeData('orig-style');
-        });
-
-        lmnt.attr('style', $(this).data('orig-style'));
-        lmnt.removeData('orig-style');
-        return this;
+    zoom: function (zoom) {
+      var self = this;
+      if (zoom === undefined) {
+        return self.ps_images.first().css('scale');
       }
-    };
 
-  $.fn.paintstripper = function (method) {
-    if (methods[method]) {
-      return methods[method].apply(this,
-        Array.prototype.slice.call(arguments, 1));
-    }
+      $(".ps-platen img").each(function () {
+        $(this).transition({ scale: (zoom / 100)}, 0);
+      });
+    },
 
-    if (typeof method === 'object' || !method) {
-      return methods.init.apply(this, arguments);
-    }
 
-    $.error('Method ' +  method + ' does not exist');
-  };
+    destroy: function () {
 
-  $.fn.save_style = function () {
-    return this.each(function () {
-      if ($(this).attr('style') === undefined) {
-        $(this).data('orig-style', $(this).attr('style'));
-      } else {
-        $(this).data('orig-style', null);
+    },
+
+    _activate_layer: function (lmnt) {
+      lmnt.addClass('active').siblings().removeClass('active');
+      return lmnt;
+    },
+
+    _init_draggable: function (lmnt) {
+      var self = this;
+
+      if (self.has_draggable === false) {
+        console.log('NO DRAG');
+        return;
       }
-    });
-  };
 
-  $.fn.init_draggable = function () {
-    if (!has_draggable) {
-      return;
-    }
+      var x1 = lmnt.offset().left,
+        y1 = lmnt.offset().top,
+        x2 = x1 + lmnt.width(),
+        y2 = y1 + lmnt.height(),
+        handle_w,
+        handle_h;
 
-    var x1 = this.offset().left,
-      y1 = this.offset().top,
-      x2 = x1 + this.width(),
-      y2 = y1 + this.height(),
-      handle_w,
-      handle_h;
-
-    this.append(ps_handle);
-    handle_w = ps_handle.outerWidth() / 2;
-    handle_h = ps_handle.outerHeight() / 2;
-    ps_handle.css({left: -handle_w, top: (this.height() - handle_h) / 2});
-    ps_handle.draggable({
-      axis: 'x',
-      containment: [x1 - handle_w, y1, x2 - handle_w, y2],
-      drag: function (e, ui) {
-        var left_offset = ui.position.left,
-          handle_offset = ui.helper.outerWidth() / 2,
-          new_offset = left_offset + handle_offset;
-        ps_shade.width(new_offset);
-      }
-    });
-
-    $(ps_platens).each(function () {
-      $(this).draggable({
-        drag: function (event, ui) {
-          $('.ps-platen').css({top: ui.position.top, left: ui.position.left});
+      lmnt.append(self.ps_handle);
+      handle_w = self.ps_handle.outerWidth() / 2;
+      handle_h = self.ps_handle.outerHeight() / 2;
+      self.ps_handle.css({left: -handle_w, top: (lmnt.height() - handle_h) / 2});
+      self.ps_handle.draggable({
+        axis: 'x',
+        containment: [x1 - handle_w, y1, x2 - handle_w, y2],
+        drag: function (e, ui) {
+          var left_offset = ui.position.left,
+            handle_offset = ui.helper.outerWidth() / 2,
+            new_offset = left_offset + handle_offset;
+          self.ps_shade.width(new_offset);
         }
       });
-    });
 
-    return this;
-  };
+      console.log(self.ps_base);
+      $.each([self.ps_base, self.ps_platen], function (i, e) {
+        console.log(e);
+        $(this).draggable({
+          drag: function (event, ui) {
+            $('.ps-platen').css({top: ui.position.top, left: ui.position.left});
+          }
+        });
+      });
 
-  $.fn.activate_layer = function () {
-    $(this).addClass('active').siblings().removeClass('active');
-    return $(this);
-  };
+      return lmnt;
+    },
 
+    _save_style: function (e) {
+      return $.each(e, function () {
+        if ($(this).attr('style') === undefined) {
+          $(this).data('orig-style', $(this).attr('style'));
+        } else {
+          $(this).data('orig-style', null);
+        }
+      });
+    },
+
+    // Move handle when windowshade is moved. Callback used in reveal()
+    _sync_handle: function (now, fx) {
+      var shade = $(this),
+        handle = shade.siblings('.ps-handle').first(),
+        current = shade.width(),
+        frame_w = shade.closest('.ps-paintstripper').width();
+
+      if (current < 0) {
+        current = 0;
+      } else if (current > frame_w) {
+        current = frame_w;
+      }
+
+      if (handle.length > 0) {
+        handle.css({left: current - (handle.outerWidth() / 2)});
+      }
+      return this;
+    }
+  });
 }(jQuery));
